98d89ee70cea5a2528cc816d366ac6f1208ecdbb
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-07-21 17:19:56 +0300

    Add db model for complaints

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Claim
 
 
 class FlatAdmin(admin.ModelAdmin):
@@ -10,4 +10,11 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('price', 'new_building', 'construction_year',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
 
+
+class ClaimAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author', 'flat',)
+    list_display = ('text',)
+
+
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Claim, ClaimAdmin)
--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -50,3 +51,10 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Claim(models.Model):
+    author = models.ForeignKey(User, verbose_name='Кто пожаловался', on_delete=models.CASCADE)
+    flat = models.ForeignKey(Flat, verbose_name='Квартира на которую пожаловались', on_delete=models.CASCADE)
+    text = models.TextField('Текст жалобы')
+
