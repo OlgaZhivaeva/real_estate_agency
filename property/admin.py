f02d28d82f0b8128500a2fe893dfce00cde73099
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-08-05 00:31:10 +0300

    Use @admin.register

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class OwnerInline(admin.TabularInline):
     raw_id_fields = ('owner',)
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address',)
     readonly_fields = ['created_at']
@@ -23,16 +24,13 @@ class FlatAdmin(admin.ModelAdmin):
     inlines = [OwnerInline, ]
 
 
+@admin.register(Claim)
 class ClaimAdmin(admin.ModelAdmin):
     raw_id_fields = ('author', 'flat',)
     list_display = ('text',)
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
     list_display = ('name', 'phonenumber', 'pure_phonenumber',)
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Claim, ClaimAdmin)
-admin.site.register(Owner, OwnerAdmin)
